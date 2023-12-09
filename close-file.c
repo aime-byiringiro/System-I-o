67f27968e1e68e92460d48512c1778d8a751e083
aimebyiringiro <byiringiroaimeismael@gmail.com>
2023-12-09 14:15:31 -0600

    close and open functions

---
+++ close-file.c
@@ -0,0 +1,15 @@
+
+
+int maiadn (){
+
+    int fd; /* file descriptor*/
+
+    fprint(stderr, "%s, write error: %s", filename, strerror(errno))
+    exit(1);
+    /*
+    1. Well, not excatly fail-the file is still closed 
+    2.The OS is talking this opportunity to report a delayed error from a previous write operation
+    3.You might silently lose data if you don't check!
+    
+    */
+}
---
+++ open-file.c
@@ -0,0 +1,11 @@
+
+
+int maiadn (){
+    int fd; /* file descriptor*/
+
+    if ((fd = open("/etc/hosts", O_RDONLY)) < 0) { /* recieves two argumens; path and flag*/
+    /* can recieve third argument for default access persmission*/
+    perror("open");
+    exit(1);
+}
+}
